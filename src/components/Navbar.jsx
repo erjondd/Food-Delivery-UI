76e123bb51e94cca02ecbef2fc4a2a6e8ff416d3
Olti Dinarama <odinarama@outlook.com>
2026-07-23 21:31:09 +0200

    Adding assets and creating components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/logo.png";
+import card from "../assets/default.png";
+import CartIcon from "../assets/CartIcon";
+const Navbar = () => {
+  return (
+    <div>
+      <img src={logo} />
+      <input type="text" placeholder="Search" />
+      <CartIcon />
+      <img src={card} />
+    </div>
+  );
+};
+
+export default Navbar;
