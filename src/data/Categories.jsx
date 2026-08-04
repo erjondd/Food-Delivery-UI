4d03e3dbe190592e2ebf0d8ec4cd8c327c9e49b9
Erjon Dinarama <erjon.dinarama@gmail.com>
2026-08-04 14:17:17 +0200

    Fix path

--- src/data/Categories.jsx
+++ src/data/Categories.jsx
@@ -2,32 +2,32 @@
 export const CATEGORIES = [
     {
         name: 'Pizza',
-        imageUrl: 'src/assets/pizza.svg',
+        imageUrl: '../assets/pizza.svg',
         active: true,
     },
     {
         name: 'Burger',
-        imageUrl: 'src/assets/burger.svg',
+        imageUrl: '../assets/burger.svg',
         active: true,
     },
     {
         name: 'BBQ',
-        imageUrl: 'src/assets/meat.svg',
+        imageUrl: '../assets/bbq.svg',
         active: false,
     },
     {
         name: 'Sushi',
-        imageUrl: 'src/assets/sushi.svg',
+        imageUrl: '../assets/sushi.svg',
         active: true,
     },
     {
         name: 'Vegan',
-        imageUrl: 'src/assets/broccoli.svg',
+        imageUrl: '../assets/broccoli.svg',
         active: false,
     },
     {
         name: 'Desert',
-        imageUrl: 'src/assets/cake.svg',
+        imageUrl: '../assets/cake.svg',
         active: false,
     }
 ]  
